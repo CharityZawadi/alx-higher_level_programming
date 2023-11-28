92205f40301b9cfad09312a3af7f4d78d6f49523
CharityZawadi <zawadicharity8@gmail.com>
2023-11-28 11:30:28 -0800

    work

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def magic_calculation(a, b, c):
+    if a < b:
+        result = c
+    elif c > b:
+        result = a + b
+    else:
+        result = a * b - c
+    return result
+
