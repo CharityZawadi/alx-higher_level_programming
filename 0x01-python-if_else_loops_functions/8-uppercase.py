3a52134f7105b34a789aa204acc385c99daabccc
CharityZawadi <zawadicharity8@gmail.com>
2023-11-28 12:41:11 -0800

    work

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,16 @@
-#!/usr/bin/python3
-def uppercase(s):
-    for char in s:
-        if 'a' <= char <= 'z':
-            print(chr(ord(char) - ord('a') + ord('A')), end="")
+#!/usr/bin/env python3
+def uppercase(str):
+    for char in str:
+        # Check if the character is a lowercase letter
+        if ord('a') <= ord(char) <= ord('z'):
+            # Convert lowercase letter to uppercase using ASCII values
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+            print("{}".format(uppercase_char), end="")
         else:
-            print(char, end="")
-    print()
+            # Print non-alphabetic characters as they are
+            print("{}".format(char), end="")
+    print()  # Print a new line at the end
 
 # Test cases
-if __name__ == "__main__":
-    uppercase("best")
-    uppercase("Best School 98 Battery street")
+uppercase("best")
+uppercase("Best School 98 Battery street")
