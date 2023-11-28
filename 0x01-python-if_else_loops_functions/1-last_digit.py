aa35bba0f96ad18806b75776a78343571c2fa5c1
CharityZawadi <zawadicharity8@gmail.com>
2023-11-28 12:11:45 -0800

    work

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,11 +8,13 @@ prefix = "Last digit of"
 comparison = "and is"
 
 if number < 0:
-    last_digit *= -1  # Make the last digit negative if the original number is negative
+    last_digit *= -1  # Make the last digit negative
+    if the original number is negative
 
 if last_digit > 5:
     print(f"{prefix} {number} is {last_digit} {comparison} greater than 5")
 elif last_digit == 0:
     print(f"{prefix} {number} is {last_digit} {comparison} 0")
 else:
-    print(f"{prefix} {number} is {last_digit} {comparison} less than 6 and not 0")
+    print(f"{prefix} {number} is {last_digit} {comparison}
+    less than 6 and not 0")
