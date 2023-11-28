fae534c01e28bb8c5c751d68acd5ce85fced6bbb
CharityZawadi <zawadicharity8@gmail.com>
2023-11-28 11:09:43 -0800

    work

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def uppercase(s):
     for char in s:
-        if ord('a') <= ord(char) <= ord('z'):
+        if 'a' <= char <= 'z':
             print(chr(ord(char) - ord('a') + ord('A')), end="")
         else:
             print(char, end="")
     print()
+
+# Test cases
+if __name__ == "__main__":
+    uppercase("best")
+    uppercase("Best School 98 Battery street")
