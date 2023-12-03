b74987f093e76f9e07c7ef70ebf03097aedc82d3
CharityZawadi <zawadicharity8@gmail.com>
2023-12-03 03:55:42 -0800

    work

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
-
-def magic_calculation(a, b):
-    add, sub = 0, ('add', 'sub')
-
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
     if a < b:
-        c = a + b
-        for i in range(4, 6):
-            c = locals()[sub[0]](c, i)
-        return c
-    else:
-        return locals()[sub[1]](a, b)
-
-# Example usage
-if __name__ == "__main__":
-    a_value = 5
-    b_value = 3
-    result = magic_calculation(a_value, b_value)
-    print(result)
-
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
