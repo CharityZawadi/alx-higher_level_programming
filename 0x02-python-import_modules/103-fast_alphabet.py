1a0191646c78a833510b5c08024c0983b733640b
CharityZawadi <zawadicharity8@gmail.com>
2023-12-04 10:44:41 -0800

    work

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-import string
-__import__('os').write(1, bytes(getattr(string, 'ascii_uppercase', string.ascii_letters), 'utf-8') + b'\n')
-
+import fast_alphabet_103
---
+++ 0x02-python-import_modules/fast_alphabet_103.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+__import__('os').write(1, bytes(range(ord('A'), ord('Z') + 1)).decode('utf-8') + '\n')
