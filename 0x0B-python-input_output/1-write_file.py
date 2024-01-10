6bf0dc9752965067698efe12eebe41a6f7dd9955
CharityZawadi <zawadicharity8@gmail.com>
2024-01-10 11:25:22 -0800

    work

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module to write to a file"""
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the number of characters written"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
+
+if __name__ == "__main__":
+    write_file()
+
