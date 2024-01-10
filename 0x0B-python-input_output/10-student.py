10f429907262ea1cfd40f2051e7f5d10472bb816
CharityZawadi <zawadicharity8@gmail.com>
2024-01-10 11:51:24 -0800

    work

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for Student class"""
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if attrs is None or not all(isinstance(attr, str) for attr in attrs):
+            return self.__dict__
+        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
