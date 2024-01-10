282d44b999dd420a217621dbcdd7842fc466d19f
CharityZawadi <zawadicharity8@gmail.com>
2024-01-10 11:07:09 -0800

    work

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a class Square that inherits from Rectangle (9-rectangle.py)."""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """Returns the string representation of the square."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
