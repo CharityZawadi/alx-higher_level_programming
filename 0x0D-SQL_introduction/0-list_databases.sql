e74e85295267fd0e124a8c139a741d23a9f58902
CharityZawadi <zawadicharity8@gmail.com>
2024-02-14 04:50:38 -0800

    work

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,10 @@
-/* List all databases */
-SHOW DATABASES;
+# ----------------------------------
+# File: 0-list_databases.sql
+# Description: Lists all databases of your MySQL server
+# Author: Guillaume
+# Date: Current date
+# ----------------------------------
+
+# List all databases
+SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA;
 
