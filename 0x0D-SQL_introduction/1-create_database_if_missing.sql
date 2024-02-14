eb7f2df9216d2b000b9ade7140085e847b855137
CharityZawadi <zawadicharity8@gmail.com>
2024-02-14 05:04:25 -0800

    work

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,10 @@
+# ----------------------------------
+# File: 1-create_database_if_missing.sql
+# Description: Creates the database hbtn_0c_0 if it does not exist
+# Author: Guillaume
+# Date: Current date
+# ----------------------------------
+
+# Create the database hbtn_0c_0 if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
