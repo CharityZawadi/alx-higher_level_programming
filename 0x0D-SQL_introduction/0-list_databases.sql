e47ff46022575e6e1a5261ced62b354173226c52
CharityZawadi <zawadicharity8@gmail.com>
2024-02-14 05:55:57 -0800

    work

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,10 +1,3 @@
-# ----------------------------------
-# File: 0-list_databases.sql
-# Description: Lists all databases of your MySQL server
-# Author: Guillaume
-# Date: Current date
-# ----------------------------------
-
-# List all databases
-SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA;
+--  a script that lists all databases of your MySQL server.
+SHOW DATABASES;
 
