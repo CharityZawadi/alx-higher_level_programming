ca3496cca09a25bc3994fbdc04568940d55fd088
CharityZawadi <zawadicharity8@gmail.com>
2024-03-12 14:17:58 -0700

    work

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// 2-rectangle.js
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      // If w or h is less than or equal to 0, create an empty object
+      return {};
+    } else {
+      // Otherwise, initialize width and height attributes
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
