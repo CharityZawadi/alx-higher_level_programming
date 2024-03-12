18d79dbd850452015b91efd5751eea7c98210588
CharityZawadi <zawadicharity8@gmail.com>
2024-03-12 14:55:23 -0700

    work

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,16 +3,13 @@
 
 class Rectangle {
   constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      // If w or h is less than or equal to 0, create an empty object
-      this.width = undefined;
-      this.height = undefined;
-    } else {
-      // Otherwise, initialize width and height attributes
-      this.width = w;
-      this.height = h;
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return {}; // Return an empty object if width or height is not positive integers
     }
+    this.width = w;
+    this.height = h;
   }
 }
 
 module.exports = Rectangle;
+
