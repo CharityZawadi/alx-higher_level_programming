a4b9c4289996792e4c3badba54f87a7ba50b07f8
CharityZawadi <zawadicharity8@gmail.com>
2024-03-22 12:53:06 -0700

    work

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,30 +7,27 @@ import MySQLdb
 
 if __name__ == "__main__":
     # Connect to MySQL server
-    conn = MySQLdb.connect(
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
+        db=sys.argv[3]
     )
 
-    # Create a cursor object using cursor() method
-    cur = conn.cursor()
+    # Create a MySQL cursor
+    cursor = db.cursor()
 
-    # Execute SQL query
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
+    # Execute the SQL query
+    cursor.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC", (sys.argv[4],))
 
-    # Fetch all the rows using fetchall() method
-    rows = cur.fetchall()
+    # Fetch all the rows
+    rows = cursor.fetchall()
 
-    # Print rows
+    # Print the rows
     for row in rows:
         print(row)
 
-    # Close cursor
-    cur.close()
-
-    # Close connection
-    conn.close()
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
