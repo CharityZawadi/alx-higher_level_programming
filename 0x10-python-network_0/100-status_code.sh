4fe5fd269bf815d1ba81b38b1418655143fb1494
CharityZawadi <zawadicharity8@gmail.com>
2024-03-27 11:59:45 -0700

    work

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -3,7 +3,4 @@
 # This script sends a request to a URL passed as an argument and displays only the status code of the response
 
 # Send a GET request to the URL passed as an argument and extract the status code from the response headers
-status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-
-# Display the extracted status code
-echo "$status_code"
+curl -s -o /dev/null -w "%{http_code}" "$1"
