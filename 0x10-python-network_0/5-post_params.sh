7229bc7ba5b1baee630098f24185e89f333ebeae
CharityZawadi <zawadicharity8@gmail.com>
2024-03-27 11:44:40 -0700

    work

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
-# This script sends a POST request to the URL passed as an argument and displays the body of the response
-# It sends variables email and subject with specified values
+#!/bin/bash
+# This script sends a POST request to the URL passed as an argument, including variables email and subject with specified values, and displays the body of the response
 curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
