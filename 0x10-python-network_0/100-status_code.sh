d4c4b0ed967f075e6b9a4651c479cca31c8b02d2
CharityZawadi <zawadicharity8@gmail.com>
2024-03-27 12:43:08 -0700

    work

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-# This script sends a request to a URL passed as an argument and displays only the status code of the response
-
-# Send a GET request to the URL passed as an argument and extract the status code from the response headers
-curl -s -o /dev/null -w "%{http_code}" "$1"
+# prints status code
+curl -so /dev/null -w "%{http_code}" $1
--- 0x10-python-network_0/101-post_json.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This script sends a request to a URL passed as an argument and displays only the status code of the response
-
-# Send a GET request to the URL passed as an argument and extract the status code from the response headers
-curl -sI "$1" | head -n 1 | cut -d' ' -f2
